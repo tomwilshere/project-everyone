8726a365e7f390cf3fa843705e4b8f81d0c21aea
Tom Wilshere <tomw@decoded.com>
2015-05-19 12:21:19 +0100

    Fixed twitter oauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:twitter, :facebook]
+         :recoverable, :rememberable, :trackable, :omniauthable, :omniauth_providers => [:twitter, :facebook]
 
   def self.from_omniauth(auth)
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0,20]
-	    user.name = auth.info.name   # assuming the user model has a name
-	    user.image = auth.info.image # assuming the user model has an image
+	    # user.name = auth.info.name   # assuming the user model has a name
+	    # user.image = auth.info.image # assuming the user model has an image
+      user.save!
 	  end
 	end
+
+  def email_required?
+    provider.blank?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20150518212047) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
