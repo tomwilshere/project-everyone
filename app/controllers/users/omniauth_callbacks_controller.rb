b395d064fdf1d3814348a8dbf5deda77460de988
Tom Wilshere <tomw@decoded.com>
2015-05-20 09:17:03 +0100

    Added facebook auth.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.image = request.env["omniauth.auth"]['info']['image']
+    @user.name = request.env["omniauth.auth"]['info']['name']
+    @user.save
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
